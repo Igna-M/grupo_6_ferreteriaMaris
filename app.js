d184f579443c862c80505d5e4c84b576f40328dc
Igna-M <ign.manzo@gmail.com>
2021-01-13 12:19:10 -0300

    Primera estructura del HTML del head en el indexy listen en el app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3030, ()=>{
+    console.log('Servidor funcionando en http://localhost:3030/');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
