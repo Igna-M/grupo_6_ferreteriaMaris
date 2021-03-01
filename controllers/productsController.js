3fc42edde740fbe5abd289d3135eedc35394ced5
Igna-M <ign.manzo@gmail.com>
2021-02-28 21:34:44 -0300

    upload product image

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,14 @@
+
 const productsController = {
     products: function(req, res) {
         res.render('products');
     },
-    
+    create:  function(req, res) {
+        console.log(req.file)
+        res.send('Archivo subido correctamente');
+        // next()
+
+    },
     
 
 }
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,31 @@
 var express = require('express');
+const multer  = require('multer');
+var path = require('path');
+
 var router = express.Router();
 
 const productsController = require('../controllers/productsController');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        // cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    //   cb(null, '${Date.now()}_img_${path.extname(file.originalname)}')
+    }
+});
+   
+const uploadFile = multer({ storage: storage })
+
+
+
 router.get('/', productsController.products);
 
+router.post('/create', uploadFile.single('product_img'), productsController.create);
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+
+
 
 module.exports = router;
