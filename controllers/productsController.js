a8348fab60ca5f4f82ba556b98579f0258fda2c0
Igna-M <ign.manzo@gmail.com>
2021-04-11 16:04:49 -0300

    CRUD completo y operativo

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -95,13 +95,8 @@ const productsController = {
     update: (req, res) => {
         
         let editarProd = productsInDB().find(producto => producto.id == req.params.id);
-        console.log('Prducto a editar:', editarProd);
-
         const errores = validationResult(req);
-        console.log(errores);
-
-        console.log('UserData:', req.body);
-
+        
         /// Validación y borrado de imagen OK. Si en el update, pongo una extensión no aceptada, avisa y borra el archivo.
         // Si en los datos actualizados encuentra un error, y subí un archivo, borra ese archivo.
         // Si no hay error, el archivo se carga (Al actualizar la base de datos, tengo que borrar el archivo anterior.)
@@ -122,75 +117,41 @@ const productsController = {
 
         }
 
+        let imageInNewProd = editarProd.image
+        if (req.file){
+            imageInNewProd = req.file.filename
 
+            let filePath = path.resolve(__dirname,'../public/images/uploads/products/' + editarProd.image);
+            console.log('Imagen borrada:', editarProd.image);
+            fs.unlinkSync(filePath);
+        } 
 
-
-        console.log('Llegamos a Update!!!');
-        datosCapturados = req.body
-        console.log(datosCapturados);
-
-        if (req.file) {
-            console.log(req.file);
+        let nuevoProducto = {
+            id: editarProd.id,
+            ...req.body,
+            image: imageInNewProd
         }
 
-   
-        // USAR MAP PARA ACTUALIZAR EL ELEMENTO DEL ARRAY
-        // . . . . . . . . . . . . . . . . . . . . . . . . .
-        //  . . . . . . . . . . . . . . . . . . . . . . . .
-        //   . . . . . . . . . . . . . . . . . . . . . . .
-        //    . . . . . . . . . . . . . . . . . . . . . .
-        //     . . . . . . . . . . . . . . . . . . . . .
+        nuevaDB = productsInDB().map(function(producto){
 
-        // let dataInDB = productsInDB()
-        // let lastElement = dataInDB[dataInDB.length -1];
-        // let lastID = lastElement.id;
-        // let nextID = lastID + 1;
+            if (producto.id == editarProd.id){
+                producto = nuevoProducto
+            }
 
-        // let nuevoProducto = {
-        //     id: nextID,
-        //     ...req.body,
-        //     image: req.file.filename
-        // }
+            return producto
 
-        // dataInDB.push(nuevoProducto);
+        })
+        
+        let uploadProducts = JSON.stringify(nuevaDB, null , 2);
+		fs.writeFileSync(productsDataDBPath, uploadProducts)
 
-        // let uploadProducts = JSON.stringify(dataInDB, null , 2);
-		// fs.writeFileSync(productsDataDBPath, uploadProducts)
+        return res.redirect('/products');
 
-        // return res.redirect('/products');
-
-
-
-        return res.send('Bien, llegaste al update!!!')
     },
     
     // Necesitamos un método para cargar en BULK a través de un csv
     // Después, puede mostrar las características de los datos y darte la opción de cargar las fotos.
-
-
-
-
-
-
-    // Update product del formulario de edición.
-    //     updateProduct: (req, res) => {
-
-    //         return res.render(req)
-
-    //     //     let editarProd = productsInDB().find(producto => producto.id == req.params.id);
-            
-    //     //     let nuevoProducto = {
-    //     //         id: editarProd.id,
-    //     //         ...req.body,
-    //     //         image: req.file.filename
-    //     //     }
-            
-    //     //  let productoSubir = JSON.stringify(productoEditado, null , 2);
-    //     //  fs.writeFileSync(productsFilePath,productoSubir);
-
-    //     //  res.redirect("/")    
-    // }
-
+    
 }
 
 module.exports = productsController
